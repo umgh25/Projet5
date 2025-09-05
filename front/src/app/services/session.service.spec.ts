c02b676753397fb114d59f79270c5471156217cf
Usman Mughal <usman.mughal@outlook.fr>
2025-09-05 11:34:13 +0200

    test(SessionService): ajout tests login/logout et $isLogged

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -2,6 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
 
 import { SessionService } from './session.service';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
+import { userRequestMock } from '../mocks/session.mocks';
+import { first } from 'rxjs';
 
 describe('SessionService', () => {
   let service: SessionService;
@@ -13,5 +16,45 @@ describe('SessionService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should log in a user', () => {
+    const user: SessionInformation = userRequestMock;
+    service.logIn(user);
+
+    expect(service.isLogged).toBe(true);
+    expect(service.sessionInformation).toEqual(user);
+  });
+
+  it('should log out a user', () => {
+    service.logOut();
+
+    expect(service.isLogged).toBe(false);
+    expect(service.sessionInformation).toBeUndefined();
+  });
+
+  it('should emit isLogged as true when user logs in', (done) => {
+    const user: SessionInformation = userRequestMock;
+
+    service.$isLogged().subscribe((isLogged) => {
+      if (isLogged) {
+        expect(isLogged).toBe(true);
+        done();
+      }
+    });
+
+    service.logIn(user);
+  });
+
+  it('should emit isLogged as false when user logs out', (done) => {
+    service
+      .$isLogged()
+      .pipe(first())
+      .subscribe((isLogged) => {
+        expect(isLogged).toBe(false);
+        done();
+      });
+
+    service.logOut();
   });
 });
