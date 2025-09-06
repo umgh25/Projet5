939ed4869d3f4cb3e836493bc9b4f5efa42a04e7
Usman Mughal <usman.mughal@outlook.fr>
2025-09-06 21:50:07 +0200

    test(UserService): ajout tests unitaires getById et delete

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,22 +1,72 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-
 import { UserService } from './user.service';
+import { User } from '../interfaces/user.interface';
+import { getUserResponseMock, userPath } from '../mocks/user.mocks';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
     });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve user by id', () => {
+    const response: User = getUserResponseMock;
+    service.getById('1').subscribe((user) => {
+      expect(user).toEqual(response);
+    });
+    const req = httpMock.expectOne(`${userPath}/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush(response);
+  });
+
+  it('should delete user by id', () => {
+    service.delete('1').subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+    const req = httpMock.expectOne(`${userPath}/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush({});
+  });
+
+  it("should handle error when id to retrieve doesn't exist", () => {
+    service.getById('1').subscribe({
+      next: () => fail('expected an error, not a user'),
+      error: (error) => expect(error.status).toBe(404),
+    });
+
+    const req = httpMock.expectOne(`${userPath}/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush('Server error', { status: 404, statusText: 'Not found' });
+  });
+
+  it("should handle error when id to delete doesn't exist", () => {
+    service.delete('1').subscribe({
+      next: () => fail('expected an error, not a confirmation'),
+      error: (error) => expect(error.status).toBe(404),
+    });
+
+    const req = httpMock.expectOne(`${userPath}/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush('Server error', { status: 404, statusText: 'Not found' });
   });
 });
