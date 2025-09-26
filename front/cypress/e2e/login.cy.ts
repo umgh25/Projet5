6af6585709ec97eca36b7abb89b4ae0bca69a1de
Usman Mughal <usman.mughal@outlook.fr>
2025-09-26 17:57:20 +0200

    test(cypress): scénarios login (succès, échec, champs vides)

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,27 +1,13 @@
 describe('Login spec', () => {
   it('Login successfull', () => {
-    cy.visit('/login')
+    cy.login({ email: 'yoga@studio.com', password: 'test!1234', type: 'user' })
+  })
 
-    cy.intercept('POST', '/api/auth/login', {
-      body: {
-        id: 1,
-        username: 'userName',
-        firstName: 'firstName',
-        lastName: 'lastName',
-        admin: true
-      },
-    })
+  it('Login failed', () => {
+    cy.login({ email: 'user@mail.com', password: 'password-1234', type: 'user' })
+  })
 
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/session',
-      },
-      []).as('session')
-
-    cy.get('input[formControlName=email]').type("yoga@studio.com")
-    cy.get('input[formControlName=password]').type(`${"test!1234"}{enter}{enter}`)
-
-    cy.url().should('include', '/sessions')
+  it('Login failed with empty fields', () => {
+    cy.login({ email: '', password: '', type: 'user' })
   })
 });
