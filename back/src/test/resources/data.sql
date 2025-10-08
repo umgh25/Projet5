e38343cb149f60c0607ddc3e23a519176d29091f
Usman Mughal <usman.mughal@outlook.fr>
2025-10-08 20:00:38 +0200

    chore(test-db): configuration H2 en mémoire + console

---
+++ back/src/test/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO TEACHERS (first_name, last_name)
+VALUES ('Margot', 'DELAHAYE'),
+       ('Hélène', 'THIERCELIN');
+
+
+INSERT INTO USERS (first_name, last_name, admin, email, password)
+VALUES ('Admin', 'Admin', true, 'yoga@studio.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq'); 
+
