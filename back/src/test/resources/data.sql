453ad8ce77686d782150306e8c1ab47284b48dcb
Usman Mughal <usman.mughal@outlook.fr>
2025-10-24 22:31:54 +0200

    test(back): config H2 + data.sql + pom

--- back/src/test/resources/data.sql
+++ back/src/test/resources/data.sql
@@ -1,8 +1,15 @@
+-- profs de test
 INSERT INTO TEACHERS (first_name, last_name)
 VALUES ('Margot', 'DELAHAYE'),
        ('Hélène', 'THIERCELIN');
 
-
+-- utilisateur admin existant
 INSERT INTO USERS (first_name, last_name, admin, email, password)
-VALUES ('Admin', 'Admin', true, 'yoga@studio.com', '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq'); 
+VALUES ('Admin', 'Admin', true, 'yoga@studio.com',
+        '$2a$10$.Hsa/ZjUVaHqi0tp9xieMeewrnZxrZ5pQRzddUXE/WjDu2ZThe6Iq');
 
+-- utilisateur non admin existant (attendu par les tests)
+-- email: user@test.com / password: password
+INSERT INTO USERS (first_name, last_name, admin, email, password)
+VALUES ('Test', 'User', false, 'user@test.com',
+        '$2a$10$7EqJtq98hPqEX7fNZaFWoOe.VeF7qZ/3GQ8LNk31DJ7tY79DbDeXO');
